2ad5a2ae114141622ea3b60cd3a15eb0d44fd561
Lee Hasiuk <lee@migo.co>
2022-08-08 14:31:01 -0400

    Fixed a bug and a compilation warning.

--- ZipZap/ZZNewArchiveEntryWriter.mm
+++ ZipZap/ZZNewArchiveEntryWriter.mm
@@ -195,10 +195,11 @@ namespace ZZDataConsumer
 
 	{
 		// if any of the blocks don't set the error, ensure we return an error anyway
+		NSError * __autoreleasing *errorPtr = error;
 		ZZScopeGuard errorChecker(^
 								  {
-									  if (error && !*error)
-										  *error = [NSError errorWithDomain:ZZErrorDomain
+									  if (errorPtr && !*errorPtr)
+										  *errorPtr = [NSError errorWithDomain:ZZErrorDomain
 																	   code:ZZBlockFailedWithoutError
 																   userInfo:nil];
 								  });
